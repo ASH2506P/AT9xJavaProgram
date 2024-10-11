d6c9f54d304d9e7f8d41a3a26675f6844afe9147
MY PC <aashi676@gmail.com>
2024-10-11 19:23:43 +0530

    all java program

---
+++ src/OCT_01102024/Table.java
@@ -0,0 +1,11 @@
+package OCT_01102024;
+
+public class Table {
+    public static void main(String[] args){
+        System.out.println("Table of  10*1=10" );
+        System.out.println("Table of  10*2=20" );
+        System.out.println("Table of  10*3=30" );
+        System.out.println("-----------------" );
+        System.out.println("Table of  10*10=100" );
+    }
+}
