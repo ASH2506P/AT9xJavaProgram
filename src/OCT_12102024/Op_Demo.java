7586b9bd777cff7bf81b321889173adef41ed800
MY PC <aashi676@gmail.com>
2024-10-14 19:37:42 +0530

    all java program

---
+++ src/OCT_12102024/Op_Demo.java
@@ -0,0 +1,7 @@
+package OCT_12102024;
+
+public class Op_Demo {
+    public static void main(String[] args){
+        System.out.println("hek");
+    }
+}
---
+++ src/OCT_14102024/TaypeCastDemo.java
@@ -0,0 +1,15 @@
+package OCT_14102024;
+
+public class TaypeCastDemo
+{
+ public static void main(String[] args)
+ {
+     int num = 100;          // int type
+     long longNum = num;    // implicit casting from int to long
+     double doubleNum = longNum; // implicit casting from long to double
+
+     System.out.println("Integer: " + num);      // Output: 100
+     System.out.println("Long: " + longNum);      // Output: 100
+     System.out.println("Double: " + doubleNum);  // Output: 100.0
+ }
+}
